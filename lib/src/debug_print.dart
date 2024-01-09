fc6d5938f76cf196230448fdb4234751799efccb
Mohamed Maher <139143509+mohamedmaher-dev@users.noreply.github.com>
2024-01-09 11:29:42 -0800

    new version

--- lib/src/debug_print.dart
+++ lib/src/debug_print.dart
@@ -1,212 +1,126 @@
 import 'package:flutter/foundation.dart';
 
-abstract class DebugPrint {
-  static void _print(String printCode) {
+const String _base = "\u001b[";
+const String _baseBack = "\u001b[0m";
+const String _empty = '';
+
+abstract class DebugPrintV2 {
+  static void _print(
+    Object msg, {
+    PrintColor? textColor,
+    PrintColor? backColor,
+    TextItalic? textItalic,
+    TextUnderline? textUnderline,
+    TextDone? textDone,
+    TextAlphaAnim? textAlphaAnim,
+    TextWeight? textWeight,
+  }) {
     if (kDebugMode) {
+      String msgCode = 'm$msg';
+      String textItalicCode = textItalic == null ? _empty : textItalic.value;
+      String textUnderlineCode =
+          textUnderline == null ? _empty : textUnderline.value;
+      String textDoneCode = textDone == null ? _empty : textDone.value;
+      String textAlphaAnimCode =
+          textAlphaAnim == null ? _empty : textAlphaAnim.value;
+      String textWeightCode = textWeight == null ? _empty : textWeight.value;
+      String textColorCode = textColor == null ? _empty : "3${textColor.value}";
+      String backColorCode = backColor == null ? _empty : "4${backColor.value}";
+      String printCode =
+          '$_base$textWeightCode$textItalicCode$textUnderlineCode$textDoneCode$textAlphaAnimCode$textColorCode$backColorCode$msgCode$_baseBack';
       print(printCode);
     }
   }
 
-  static void normal(String msg) {
-    _print('\u001b[0m$msg');
-  }
-
-  static void success(String msg) {
-    _print('\u001b[32m$msg');
-  }
-
-  static void successBack(String msg) {
-    _print('\u001b[30;42m$msg\u001b[0m');
-  }
-
-  static void error(String msg) {
-    _print('\u001b[31m$msg');
-  }
-
-  static void errorBack(String msg) {
-    _print('\u001b[30;41m$msg\u001b[0m');
-  }
-
-  static void warning(String msg) {
-    _print('\u001b[33m$msg');
-  }
-
-  static void warningBack(String msg) {
-    _print('\u001b[30;43m$msg\u001b[0m');
-  }
-
-  static void black(String msg) {
-    _print('\u001b[30m$msg');
-  }
-
-  static void blue(String msg) {
-    _print('\u001b[34m$msg');
-  }
-
-  static void white(String msg) {
-    _print('\u001b[37m$msg');
-  }
-
-  static void magenta(String msg) {
-    _print('\u001b[35m$msg');
-  }
-
-  static void cyan(String msg) {
-    _print('\u001b[36m$msg');
-  }
-
+  static void normal(Object msg) => _print(msg);
+  static void success(Object msg) => _print(msg, textColor: PrintColor.green);
+  static void error(Object msg) => _print(msg, textColor: PrintColor.red);
+  static void warning(Object msg) => _print(msg, textColor: PrintColor.yellow);
+  static void black(Object msg) => _print(msg, textColor: PrintColor.black);
+  static void blue(Object msg) => _print(msg, textColor: PrintColor.blue);
+  static void white(Object msg) => _print(msg, textColor: PrintColor.white);
+  static void magenta(Object msg) => _print(msg, textColor: PrintColor.magenta);
+  static void cyan(Object msg) => _print(msg, textColor: PrintColor.cyan);
+  static void successBack(Object msg) =>
+      _print(msg, textColor: PrintColor.black, backColor: PrintColor.green);
+  static void warningBack(Object msg) =>
+      _print(msg, textColor: PrintColor.black, backColor: PrintColor.yellow);
+  static void errorBack(Object msg) =>
+      _print(msg, textColor: PrintColor.black, backColor: PrintColor.red);
   static void custom(
-    String msg, {
+    Object msg, {
+    PrintColor? textColor,
+    PrintColor? backColor,
+    TextItalic? textItalic,
+    TextUnderline? textUnderline,
+    TextDone? textDone,
+    TextAlphaAnim? textAlphaAnim,
     TextWeight? textWeight,
-    bool? isItalic,
-    bool? isUnderline,
-    bool? isDone,
-    TextAlpha? textAlpha,
-    ColorList? textColor,
-    ColorList? backColor,
-  }) {
-    late String weight;
-    late String italic;
-    late String underline;
-    late String done;
-    late String alpha;
-    late String colorText;
-    late String colorBack;
-
-    if (textWeight == null) {
-      weight = "";
-    } else {
-      switch (textWeight) {
-        case TextWeight.normal:
-          {
-            weight = "0;";
-          }
-        case TextWeight.bold:
-          {
-            weight = "1;";
-          }
-      }
-    }
-
-    if (isItalic == null || !isItalic) {
-      italic = "";
-    } else {
-      italic = "3;";
-    }
-
-    if (isUnderline == null || !isUnderline) {
-      underline = "";
-    } else {
-      underline = "4;";
-    }
-
-    if (isDone == null || !isDone) {
-      done = "";
-    } else {
-      done = "9;";
-    }
-
-    if (textAlpha == null) {
-      alpha = "";
-    } else {
-      alpha = "${_getAlphaCode(textAlpha)};";
-    }
-
-    if (textColor == null) {
-      colorText = "";
-    } else {
-      colorText = "3${_getColorCode(textColor)};";
-    }
-
-    if (backColor == null) {
-      colorBack = "";
-    } else {
-      colorBack = "4${_getColorCode(backColor)};";
-    }
-
-    _print(
-        '\u001b[$weight$italic$underline$done$alpha$colorText${colorBack}m$msg\u001b[0m');
-  }
-
-  static String _getColorCode(ColorList color) {
-    switch (color) {
-      case ColorList.black:
-        {
-          return "0";
-        }
-      case ColorList.red:
-        {
-          return "1";
-        }
-      case ColorList.green:
-        {
-          return "2";
-        }
-
-      case ColorList.yellow:
-        {
-          return "3";
-        }
-      case ColorList.blue:
-        {
-          return "4";
-        }
-      case ColorList.magenta:
-        {
-          return "5";
-        }
-      case ColorList.cyan:
-        {
-          return "6";
-        }
-      case ColorList.white:
-        {
-          return "7";
-        }
-    }
-  }
-
-  static String _getAlphaCode(TextAlpha alpha) {
-    switch (alpha) {
-      case TextAlpha.semi:
-        {
-          return "2";
-        }
-      case TextAlpha.full:
-        {
-          return "8";
-        }
-      case TextAlpha.slow:
-        {
-          return "5";
-        }
-      case TextAlpha.fast:
-        {
-          return "6";
-        }
-    }
-  }
+  }) =>
+      _print(
+        msg,
+        backColor: backColor,
+        textAlphaAnim: textAlphaAnim,
+        textColor: textColor,
+        textDone: textDone,
+        textItalic: textItalic,
+        textUnderline: textUnderline,
+        textWeight: textWeight,
+      );
 }
 
 enum TextWeight {
-  bold,
-  normal,
+  normal(value: "0;"),
+  bold(value: "1;");
+
+  final String value;
+  const TextWeight({required this.value});
 }
 
-enum TextAlpha {
-  semi,
-  full,
-  slow,
-  fast,
+enum TextAlphaAnim {
+  semiOpacity(value: "2;"),
+  hide(value: "8;"),
+  slowAnim(value: "5;"),
+  fastAnim(value: "6;");
+
+  final String value;
+  const TextAlphaAnim({required this.value});
 }
 
-enum ColorList {
-  black,
-  red,
-  green,
-  yellow,
-  blue,
-  magenta,
-  cyan,
-  white,
+enum TextItalic {
+  none(value: _empty),
+  italic(value: "3;");
+
+  final String value;
+  const TextItalic({required this.value});
+}
+
+enum TextUnderline {
+  none(value: _empty),
+  underline(value: "4;");
+
+  final String value;
+  const TextUnderline({required this.value});
+}
+
+enum TextDone {
+  none(value: _empty),
+  done(value: "9;");
+
+  final String value;
+  const TextDone({required this.value});
+}
+
+enum PrintColor {
+  black(value: "0;"),
+  red(value: "1;"),
+  green(value: "2;"),
+  yellow(value: "3;"),
+  blue(value: "4;"),
+  magenta(value: "5;"),
+  cyan(value: "6;"),
+  white(value: "7;");
+
+  final String value;
+  const PrintColor({required this.value});
 }
--- test/debug_print_flutter_test.dart
+++ test/debug_print_flutter_test.dart
@@ -1,7 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:debug_print_flutter/debug_print_flutter.dart';
-
 void main() {
   test('adds one to input values', () {
     final calculator = Calculator();
